b05dc78bc75971c172c112e8efcdd0dec3a20fdc
markus-moser <markus.moser@pimcore.com>
2025-04-23 14:03:14 +0200

    Add custom image asset toolbar example (#1)

---
+++ assets/js/src/examples/asset-editor-toolbar-button/index.ts
@@ -0,0 +1,19 @@
+import { type AbstractModule, container } from '@pimcore/studio-ui-bundle'
+import { serviceIds } from '@pimcore/studio-ui-bundle/app'
+import { componentConfig, type ComponentRegistry } from '@pimcore/studio-ui-bundle/modules/app'
+import CustomToolbarButton from './components/custom-toolbar-button/custom-toolbar-button'
+
+export const AssetEditorToolbarButtonExtension: AbstractModule = {
+  onInit: (): void => {
+    const componentRegistry = container.get<ComponentRegistry>(serviceIds['App/ComponentRegistry/ComponentRegistry'])
+
+    componentRegistry.registerToSlot(
+      componentConfig.asset.editor.toolbar.slots.left.name,
+      {
+        name: 'customToolbarButton',
+        component: CustomToolbarButton,
+        priority: 500
+      }
+    )
+  }
+}
