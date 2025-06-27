e8e7d10970d5e67ff6dfba91464406658befc9c6
vin0401 <26813978+vin0401@users.noreply.github.com>
2025-06-27 13:50:16 +0000

    Automatic frontend build

--- public/build/473fab6c-6a4f-4010-a9f9-55d545e0c21f/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-      if (window.pluginRemotes === undefined) {
-        window.pluginRemotes = {}
-      }
-
-      window.pluginRemotes.pimcore_studio_example_bundle = "/bundles/pimcorestudioexample/build/473fab6c-6a4f-4010-a9f9-55d545e0c21f/static/js/remoteEntry.js"
-    
---
+++ public/build/dd2b229f-1843-41da-b5c1-30c921e880aa/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_studio_example_bundle = "/bundles/pimcorestudioexample/build/dd2b229f-1843-41da-b5c1-30c921e880aa/static/js/remoteEntry.js"
+    
