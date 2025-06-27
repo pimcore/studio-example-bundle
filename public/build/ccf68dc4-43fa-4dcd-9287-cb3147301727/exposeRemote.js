15d785f6b13e22a04d7ec1ff4bb76ca493fdb85b
vin0401 <26813978+vin0401@users.noreply.github.com>
2025-06-27 09:21:18 +0000

    Automatic frontend build

--- public/build/ccf68dc4-43fa-4dcd-9287-cb3147301727/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-      if (window.pluginRemotes === undefined) {
-        window.pluginRemotes = {}
-      }
-
-      window.pluginRemotes.pimcore_studio_example_bundle = "/bundles/pimcorestudioexample/build/ccf68dc4-43fa-4dcd-9287-cb3147301727/static/js/remoteEntry.js"
-    
