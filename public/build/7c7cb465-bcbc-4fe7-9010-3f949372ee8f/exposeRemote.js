d5004a7d6e61d154561e952f9639eebd12f103a8
vin0401 <marvin.bertram@pimcore.com>
2025-06-27 11:13:17 +0200

    Add npm package

---
+++ public/build/7c7cb465-bcbc-4fe7-9010-3f949372ee8f/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_studio_example_bundle = "/bundles/pimcorestudioexample/build/7c7cb465-bcbc-4fe7-9010-3f949372ee8f/static/js/remoteEntry.js"
+    
