15ded104b9fd79312c314ab9c2e3c46c66b22d6b
Marvin Bertram <marvin.bertram@pimcore.com>
2026-02-11 11:15:25 +0100

    Update package.json (#20)

---
+++ public/build/6ec6f6b9-602c-4063-9a09-3ff3a3c38ce8/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_studio_example_bundle = "http://localhost:3032/static/js/remoteEntry.js"
+    
