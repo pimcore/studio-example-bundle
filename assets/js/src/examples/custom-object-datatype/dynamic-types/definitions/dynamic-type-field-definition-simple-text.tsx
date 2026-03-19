5624159a9c65b29852c6a7c0344894c0e6b0eb9c
Christian Fasching <christian.fasching@pimcore.com>
2026-03-19 18:57:53 +0100

    added object data type example

--- assets/js/src/examples/custom-object-datatype/dynamic-types/definitions/dynamic-type-field-definition-simple-text.tsx
+++ assets/js/src/examples/custom-object-datatype/dynamic-types/definitions/dynamic-type-field-definition-simple-text.tsx
@@ -7,14 +7,21 @@ import { type ElementIcon } from '@pimcore/studio-ui-bundle/modules/widget-manag
 export class DynamicTypeFieldDefinitionSimpleText extends DynamicTypeFieldDefinitionDataAbstract {
   id: string = 'simpleText'
 
+  // Icon shown in the "Add Data Component" dropdown of the class editor.
   getIcon (): ElementIcon {
     return { type: 'name', value: 'text-input' }
   }
 
+  // Places this type in the "Text" category of the dropdown.
+  // The base class already returns ['data'].
   getGroup (): string[] {
     return [...super.getGroup(), 'text']
   }
 
+  // Type-specific settings shown in the "Specific Settings" panel
+  // of the class editor when this field is selected.
+  // Return form fields here (e.g. column length, regex pattern).
+  // Empty fragment means no type-specific settings.
   getSpecificFormFields (): React.JSX.Element {
     return <></>
   }
--- assets/js/src/examples/custom-object-datatype/dynamic-types/definitions/dynamic-type-object-data-simple-text.tsx
+++ assets/js/src/examples/custom-object-datatype/dynamic-types/definitions/dynamic-type-object-data-simple-text.tsx
@@ -1,4 +1,5 @@
 import { injectable } from 'inversify'
+import type React from 'react'
 import { container } from '@pimcore/studio-ui-bundle'
 import { serviceIds } from '@pimcore/studio-ui-bundle/app'
 import { DynamicTypeObjectDataAbstractInput } from '@pimcore/studio-ui-bundle/modules/element'
@@ -7,7 +8,19 @@ import { DynamicTypeObjectDataAbstractInput } from '@pimcore/studio-ui-bundle/mo
 export class DynamicTypeObjectDataSimpleText extends DynamicTypeObjectDataAbstractInput {
   id: string = 'simpleText'
 
+  // Enables the string filter in the grid sidebar.
+  // Default is 'DynamicTypes/FieldFilter/None' (filtering disabled).
+  // The filter type determines both the filter UI component and the
+  // backend filter used (here: system.string -> wildcard search).
   readonly dynamicTypeFieldFilterType: any = container.get(serviceIds['DynamicTypes/FieldFilter/String'])
+
+  // Renders the field in the data object editor.
+  // The abstract input class provides a default <Input> component.
+  // Override this to render a custom component instead.
+  getObjectDataComponent (props: any): React.ReactElement {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return super.getObjectDataComponent(props)
+  }
 
   getDefaultGridColumnWidth (): number | undefined {
     return 350
